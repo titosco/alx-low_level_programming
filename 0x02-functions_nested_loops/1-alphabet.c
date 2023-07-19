cde1c36b6007dda093a4a467128b10858e021db1
titosco <titobiloluwatella@gmail.com>
2023-07-18 17:20:55 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_alphabet - Entry point
+ *Description: 'print_alphabet'
+ *Return: always 0 (success)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+}
